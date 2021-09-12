f0dbe259ff6cb4c64fde22390fd4bf6f3abec464
mihmilicio <37940750+mihmilicio@users.noreply.github.com>
2021-09-12 16:51:47 -0300

    GetById, Delete, DeleteByName e Update de Produto; Implementação de códigos http

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using BaseDsvApi.Data;
@@ -15,15 +16,72 @@ namespace BaseDsvApi.Controllers
 
         [HttpPost]
         [Route("create")]
-        public Produto Create(Produto produto)
+        public IActionResult Create([FromBody] Produto produto)
         {
             _context.Produtos.Add(produto);
             _context.SaveChanges();
-            return produto;
+            return Created("", produto);
         }
 
         [HttpGet]
         [Route("list")]
         public List<Produto> List() => _context.Produtos.ToList();
+        
+        [HttpGet]
+        [Route("find/{produtoId}")]
+        public IActionResult Find([FromRoute] int produtoId)
+        {
+            Produto produto = _context.Produtos.Find(produtoId);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(produto);
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public IActionResult Update([FromBody] Produto produto)
+        {
+            try
+            {
+                _context.Produtos.Update(produto);
+                _context.SaveChanges();
+                return Ok(produto);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+        
+        [HttpDelete]
+        [Route("delete/{produtoId}")]
+        public IActionResult Delete([FromRoute] int produtoId)
+        {
+            Produto produto = _context.Produtos.Find(produtoId);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok(produto); // pode fazer sentido retornar a lista atualizada
+        }
+        
+        [HttpDelete]
+        [Route("deletebyname/{produtoNome}")]
+        public IActionResult DeleteByName([FromRoute] string produtoNome)
+        {
+            Produto produto = _context.Produtos.FirstOrDefault(p => p.Nome == produtoNome);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok(_context.Produtos.ToList());
+        }
     }
 }
